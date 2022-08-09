16d3946b8fa4e19fe89f558f402d0c8d56e56ec8
John Hall <john.hall@ruralinnovation.us>
2022-08-09 14:43:02 -0400

    add amplify yaml build config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   external: [ 'aws_amplify', 'Buffer', 'CustomEvent', 'HTMLWidgets', 'Shiny', 'shinyjs' ],
-  plugins: [react()]
-})
+  plugins: [
+      react()
+  ]
+});
