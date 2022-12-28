567c9e11d9adab7da1641500e2ef9b9dbf4ca67e
John Hall <john.hall@ruralinnovation.us>
2022-12-28 13:04:44 -0500

    restore amplify folder for cori risi dev

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,15 @@
+export type AmplifyDependentResourcesAttributes = {
+    "hosting": {
+        "ElasticContainer": {
+            "ServiceName": "string",
+            "ClusterName": "string",
+            "PipelineName": "string",
+            "ContainerNames": "string",
+            "PipelineUrl": "string",
+            "LoadBalancerAliasDomainName": "string",
+            "LoadBalancerCnameDomainName": "string",
+            "CloudfrontDistributionAliasDomainName": "string",
+            "CloudfrontDistributionCnameDomainName": "string"
+        }
+    }
+}
