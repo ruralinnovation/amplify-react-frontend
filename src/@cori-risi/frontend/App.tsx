d869bf10a153be34719bb7d04f66d5739e2900b2
John Hall <john.hall@ruralinnovation.us>
2023-12-20 00:47:27 -0500

    toggle menu kind of works

--- src/@cori-risi/frontend/App.tsx
+++ src/@cori-risi/frontend/App.tsx
@@ -16,11 +16,23 @@ import './App.css';
 function App({ content }: { content: () => HTMLElement }): ReactElement {
     let content_loaded = false;
     
-    const [windowWidth, setWidth]   = useState(0);
-    const [windowHeight, setHeight] = useState(0);
-    const [windowRatio, setRatio] = useState(0);
+    const [ windowWidth, setWidth ]   = useState(0);
+    const [ windowHeight, setHeight ] = useState(0);
+    const [ windowRatio, setRatio ] = useState(0);
     
-    const [count, setCount] = useState(0);
+    const [ count, setCount ] = useState(0);
+    
+    const [ controlPanelOpen, setControlPanelOpen ] = useState(false);
+    
+    let toggleControlPanel = function() {
+        if (!controlPanelOpen) {
+            setControlPanelOpen(true);
+            return false;
+        } else {
+            setControlPanelOpen(false);
+            return true;
+        }
+    };
     
     function updateCartBy (quantity: number) {
         setCount(count + quantity);
@@ -80,15 +92,14 @@ function App({ content }: { content: () => HTMLElement }): ReactElement {
         // Check access to react/vite environment variables
         // console.log("Welcome to Amplify React app version:", import.meta.env.VITE_APP_VERSION);
         // console.log(aws_config);
-    }())
+    }());
     
     return (
         <>
-            {/*<Flex direction="row"*/}
-            {/*      justifyContent="space-between" >*/}
-            {/*    */}
-            {/*    <Flex direction="column">*/}
-                    <ControlPanel/>
+            <Flex direction="row"
+                  justifyContent="space-between" >
+                
+                <Flex direction="column" flex={(controlPanelOpen)? "initial" : "auto"}>
                     <h1 style={{textAlign: "center"}}>Vite + React + TS</h1>
                     <br />
                     <Flex
@@ -136,24 +147,26 @@ function App({ content }: { content: () => HTMLElement }): ReactElement {
                     <p className="read-the-docs" style={{textAlign: "center"}}>
                         Click on the Vite and React logos to learn more
                     </p>
-            {/*    </Flex>*/}
-            {/*    */}
-            {/*    <div className={"amplify-sign-out"}><SignOutButton /></div>*/}
-            {/*    */}
-            {/*</Flex>*/}
+                </Flex>
+    
+                <ControlPanel isOpen={controlPanelOpen} toggle={toggleControlPanel} />
+                {/*<div className={"amplify-sign-out"}><SignOutButton /></div>*/}
+                
+            </Flex>
         </>
     )
 }
 
-function SignOutButton () {
-    const { signOut } = useAuthenticator();
+function SignOutButton ({ signOut }) {
     return <Button title="Sign Out" onClick={signOut}>Sign Out</Button>;
 }
 
 function ControlPanel (props) {
-    const signOut = (props.hasOwnProperty("signOut")) ?
-        props["signOut"] :
+    const { signOut } = (props.hasOwnProperty("signOut")) ?
+        { signOut: props["signOut"] }:
         useAuthenticator();
+    const [ open, setOpen ] = useState((props.hasOwnProperty("isOpen")) ? props["isOpen"] : true);
+    const toggle = props.toggle;
     const user = props.user;
     
     useEffect(() => {
@@ -161,60 +174,59 @@ function ControlPanel (props) {
     }, [ props.user ]);
     
     return (
-        <div className="control-panel controls">
+        <div className="control-panel">
+            <div className="menu-toggle col">
+                <a className="menu-toggle__control js-menu-control js-open-main-menu" role="button" >
+                    <span id="mm-label" className="hamburger-control__label">
+                      <span className="hamburger-control__open-label" aria-hidden={ !open } style={{ display: open ? "none" : "block" }} >
+                        <span className="screen-reader-text">Site Menu</span>
+                      </span>
+                      <span className="hamburger-control__close-label" aria-hidden={ open }>
+                        <span className="screen-reader-text" onClick={() => { setOpen(!open); return toggle() }}>Close Menu</span>
+                      </span>
+                    </span>
+                    <span className="hamburger-control" aria-hidden={ !open }>
+                        <span className="hamburger-control__inner"/>
+                        <span className="hamburger-control__inner"/>
+                        <span className="hamburger-control__open" aria-hidden={ !open }
+                              onClick={() => { setOpen(!open); return toggle() }}
+                              style={{ display: open ? "none" : "block"  }} >
+                            <svg className="menu" width="20" height="18" viewBox="0 0 20 18" fill="none"
+                                 xmlns="http://www.w3.org/2000/svg">
+                                <g fill="#16343e">
+                                    <rect y=".5" width="20" height="3" rx="1.5" />
+                                    <rect y="7.5" width="20" height="3" rx="1.5" />
+                                    <rect y="14.5" width="20" height="3" rx="1.5" />
+                                </g><defs><clipPath id="clip0">
+                                <path fill="#fff" transform="translate(0 .5)"
+                                      d="M0 0h20v17H0z"/></clipPath></defs>
+                            </svg>
+                        </span>
+                        <span className="hamburger-control__close" aria-hidden={ open }
+                              onClick={() => { setOpen(!open); return toggle() }}
+                              style={{ display: open ? "block" : "none"  }} >
+                            <svg className="menu-close" width="22" height="22" viewBox="0 0 22 22" fill="none"
+                                 xmlns="http://www.w3.org/2000/svg">
+                                <path
+                                    d="M3.304 20.801L11 13.104l7.698 7.697c.296.297.671.449 1.052.449.38 0 .756-.152 1.052-.449a1.459 1.459 0 000-2.104L13.104 11 20.8 3.303a1.459 1.459 0 000-2.104 1.46 1.46 0 00-2.104 0h0L11 8.898 3.301 1.2a1.46 1.46 0 00-2.103 0h0a1.46 1.46 0 000 2.104L8.834 11 1.2 18.697s0 0 0 0a1.458 1.458 0 00-.012 2.092c.255.326.713.461 1.064.461.38 0 .756-.152 1.052-.449h0z"
+                                    fill="{!!light_on_dark ? '#fffff9' : '#16343e'}" stroke="#16343e"
+                                />
+                            </svg>
+                        </span>
+                    </span>
+                </a>
+            </div>
             
             {(user !== null) ? (
-                <div>
+                <div className="controls" style={open ? {
+                    maxWidth: "min-content",
+                    paddingLeft: "56px",
+                    paddingRight: "56px"
+                } : {
+                    maxWidth: "0px",
+                    padding: "0px"
+                }}>
     
-                    <div id={"auth-control"} className="row show">
-                        {/*        {(signOut !== null) ? (*/}
-                        {/*            // <button id={"sign-out"} className={"amplify-button amplify-field-group__control amplify-button--primary amplify-button--fullwidth btn btn-primary btn-lg"} onClick={() => { autoSignIn(); signOut(); }}>Sign out</button>*/}
-                        <div className="site-header__item menu-toggle col">
-                            <a className="menu-toggle__control js-menu-control js-open-main-menu" role="button" >
-                                    <span id="mm-label" className="hamburger-control__label">
-                                      <span className="hamburger-control__open-label" aria-hidden="false" style={{ display: "none" }}>
-                                        <span className="screen-reader-text">Site Menu</span>
-                                      </span>
-                                      <span className="hamburger-control__close-label" aria-hidden="true">
-                                        <span className="screen-reader-text">Close Menu</span>
-                                      </span>
-                                    </span>
-                                <span className="hamburger-control"
-                                      aria-hidden="true">
-                                        <span className="hamburger-control__inner"/>
-                                        <span className="hamburger-control__inner"/>
-                                        <span className="hamburger-control__open" aria-hidden="false" style={{ display: "none" }} >
-                                            <svg className="menu" width="20" height="18" viewBox="0 0 20 18" fill="none"
-                                                 xmlns="http://www.w3.org/2000/svg">
-                                                <g fill="#16343e">
-                                                    <rect y=".5" width="20" height="3" rx="1.5" />
-                                                    <rect y="7.5" width="20" height="3" rx="1.5" />
-                                                    <rect y="14.5" width="20" height="3" rx="1.5" />
-                                                </g><defs><clipPath id="clip0">
-                                                <path fill="#fff" transform="translate(0 .5)"
-                                                      d="M0 0h20v17H0z"/></clipPath></defs>
-                                            </svg>
-                                        </span>
-                                        <span className="hamburger-control__close" aria-hidden="true">
-                                            <svg className="menu-close" width="22" height="22" viewBox="0 0 22 22" fill="none"
-                                                 xmlns="http://www.w3.org/2000/svg">
-                                                <path
-                                                    d="M3.304 20.801L11 13.104l7.698 7.697c.296.297.671.449 1.052.449.38 0 .756-.152 1.052-.449a1.459 1.459 0 000-2.104L13.104 11 20.8 3.303a1.459 1.459 0 000-2.104 1.46 1.46 0 00-2.104 0h0L11 8.898 3.301 1.2a1.46 1.46 0 00-2.103 0h0a1.46 1.46 0 000 2.104L8.834 11 1.2 18.697s0 0 0 0a1.458 1.458 0 00-.012 2.092c.255.326.713.461 1.064.461.38 0 .756-.152 1.052-.449h0z"
-                                                    fill="{!!light_on_dark ? '#fffff9' : '#16343e'}" stroke="#16343e"
-                                                />
-                                            </svg>
-                                        </span>
-                                    </span>
-                            </a>
-                            <SignOutButton />
-                        </div>
-                        {/*        ) : (*/}
-                        {/*            <button>No Auth Controls</button>*/}
-                        {/*        )*/}
-                        {/*        }*/}
-    
-                    </div>
-                    
                     {/*<h5>{*/}
                     {/*    (user.hasOwnProperty("signInUserSession") && user.signInUserSession.hasOwnProperty("idToken") && user.signInUserSession.idToken.hasOwnProperty("payload")) ? (*/}
                     {/*            (user.signInUserSession.idToken.payload.hasOwnProperty("name") && !!user.signInUserSession.idToken.payload.name) ?*/}
@@ -227,7 +239,7 @@ function ControlPanel (props) {
                     {/*            user.email :*/}
                     {/*            user.username*/}
                     {/*}</h5>*/}
-                    
+    
                     {/*<br />*/}
 
                     <p id="info">&nbsp;</p>
@@ -341,6 +353,15 @@ function ControlPanel (props) {
                         <button type="submit" className={"amplify-button amplify-field-group__control amplify-button--primary amplify-button--fullwidth btn btn-primary btn-lg"} id={"config-btn"}>Show print options</button>
                         <button type="submit" className={"amplify-button amplify-field-group__control amplify-button--primary amplify-button--fullwidth btn btn-primary btn-lg"} id={"generate-btn"}>Print report</button>
                         <div id='spinner' />
+                    </div>
+    
+                    <div id={"auth-control"} className="row show">
+                        {(signOut !== null) ? (
+                            // <button id={"sign-out"} className={"amplify-button amplify-field-group__control amplify-button--primary amplify-button--fullwidth btn btn-primary btn-lg"} onClick={() => { autoSignIn(); signOut(); }}>Sign out</button>
+                            <SignOutButton signOut={signOut} />
+                        ) : (
+                            <button>No Auth Controls</button>
+                        )}
                     </div>
                     
                 </div>
