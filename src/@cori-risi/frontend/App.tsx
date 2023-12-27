024f5ea1a2a48d6d7a5cc06df1d04bf649aee24b
John Hall <john.hall@ruralinnovation.us>
2023-12-27 12:43:20 -0500

    move menu function out of control panel and into own component

--- src/@cori-risi/frontend/App.tsx
+++ src/@cori-risi/frontend/App.tsx
@@ -162,7 +162,9 @@ function App({ content, user }: { content: () => HTMLElement, user: Promise<User
                     </p>
                 </Flex>
 
-                <ControlPanel showMenuButton={allowMenuToBeClosed} toggleCallback={toggleControlPanel} />
+                <ControlPanel showMenuButton={allowMenuToBeClosed} toggleCallback={toggleControlPanel}>
+                    <ApplicationMenu />
+                </ControlPanel>
                 {/*<div className={"amplify-sign-out"}><SignOutButton /></div>*/}
 
             </Flex>
@@ -170,7 +172,138 @@ function App({ content, user }: { content: () => HTMLElement, user: Promise<User
     );
 }
 
+function ApplicationMenu() {
+
+    function showPrintOptions() {
+        try {
+            const print_config_form = document.getElementById("print-config")!;
+            print_config_form.className = "show";
+            const print_config_button = document.getElementById("print-config-btn")!;
+            print_config_button.className = "amplify-button hide";
+        } catch (e: unknown) {}
+    }
+
+    return (
+        <div id={"application-menu"}>
+            <div id={"print-exec"} className="row">
+                <Button type="submit"  id={"print-config-btn"}
+                        className={"amplify-button amplify-field-group__control amplify-button--primary amplify-button--fullwidth btn btn-primary btn-lg"}
+                        onClick={showPrintOptions}>
+                    Show print options</Button>
+                {/* */}
+                {/* Print config form */}
+                {/* */}
+                <form id={"print-config"}>
+                    <fieldset id="config-fields">
+                        <div className="row">
+                            <div className="col-sm-4">
+                                <div className="form-group">
+                                    <label>Unit</label><br />
+                                    <label className="radio-inline">
+                                        <input type="radio" name="unitOptions" value="in" id="inUnit" checked readOnly /> Inch
+                                    </label>
+                                    <label className="radio-inline">
+                                        <input type="radio" name="unitOptions" value="mm" id="mmUnit" readOnly /> Millimeter
+                                    </label>
+                                </div>
+                            </div>
+                            <div className="col-sm-3">
+                                <div className="form-group">
+                                    <label>Output format</label><br />
+                                    <label className="radio-inline">
+                                        <input type="radio" name="outputOptions" value="png" checked readOnly /> PNG
+                                    </label>
+                                    <label className="radio-inline">
+                                        <input type="radio" name="outputOptions" value="pdf" readOnly /> PDF
+                                    </label>
+                                </div>
+                            </div>
+                            <div className="col-sm-5">
+                                <div className="form-group">
+                                    <label htmlFor="styleSelect">Map style</label>
+                                    <select id="styleSelect" className="form-control">
+                                        <option value="mapbox://styles/mapbox/light-v9">Mapbox Light</option>
+                                        <option value="mapbox://styles/mapbox/streets-v10">Mapbox Streets</option>
+                                        <option value="https://tiles.stadiamaps.com/styles/alidade_smooth.json">Stadia Maps
+                                            Alidade Smooth
+                                        </option>
+                                        <option value="https://tiles.stadiamaps.com/styles/alidade_smooth_dark.json">Stadia
+                                            Maps Alidade Smooth Dark
+                                        </option>
+                                        <option value="https://tiles.stadiamaps.com/styles/outdoors.json">Stadia Maps
+                                            Outdoors
+                                        </option>
+                                        <option value="https://tiles.stadiamaps.com/styles/osm_bright.json">Stadia Maps OSM
+                                            Bright
+                                        </option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col-sm-2">
+                                <div className="form-group" id="widthGroup">
+                                    <label htmlFor="widthInput">Width</label>
+                                    <input type="text" className="form-control" id="widthInput" autoComplete="off"
+                                           readOnly value="8" />
+                                </div>
+                            </div>
+                            <div className="col-sm-2">
+                                <div className="form-group" id="heightGroup">
+                                    <label htmlFor="heightInput">Height</label>
+                                    <input type="text" className="form-control" id="heightInput" autoComplete="off"
+                                           readOnly value="6" />
+                                </div>
+                            </div>
+                            <div className="col-sm-3">
+                                <div className="form-group" id="dpiGroup">
+                                    <label htmlFor="dpiInput">DPI</label>
+                                    <input type="text" className="form-control" id="dpiInput" autoComplete="off"
+                                           readOnly value="300" />
+                                </div>
+                            </div>
+                            <div className="col-sm-5">
+                                <div className="row">
+                                    <div className="col-sm-4">
+                                        <div className="form-group" id="latGroup">
+                                            <label htmlFor="latInput">Latitude</label>
+                                            <input type="text" className="form-control" id="latInput" autoComplete="off"
+                                                   readOnly value="" />
+                                        </div>
+                                    </div>
+                                    <div className="col-sm-4">
+                                        <div className="form-group" id="lonGroup">
+                                            <label htmlFor="lonInput">Longitude</label>
+                                            <input type="text" className="form-control" id="lonInput" autoComplete="off"
+                                                   readOnly value="" />
+                                        </div>
+                                    </div>
+                                    <div className="col-sm-4">
+                                        <div className="form-group" id="zoomGroup">
+                                            <label htmlFor="zoomInput">Zoom</label>
+                                            <input type="text" className="form-control" id="zoomInput" autoComplete="off"
+                                                   readOnly value="" />
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </fieldset>
+                </form>
+
+                <button type="submit" className={"amplify-button amplify-field-group__control amplify-button--primary amplify-button--fullwidth btn btn-primary btn-lg"} id={"generate-btn"}>Print report</button>
+                <div id='spinner' />
+            </div>
+
+            <a id={"data-download-link"} className="row">
+                <button type="submit" className={"amplify-button amplify-field-group__control amplify-button--primary amplify-button--fullwidth btn btn-primary btn-lg"} id={"data-download-btn"}>Download data</button>
+            </a>
+        </div>
+    );
+}
+
 function ControlPanel (props: {
+    children?: ReactElement,
     showMenuButton?: boolean | null,
     toggleCallback?: Function | null,
     signOut?: Function | null,
@@ -205,15 +338,6 @@ function ControlPanel (props: {
             (u.hasOwnProperty("email") && !!u.email) ?
                 u.email :
                 u.username
-    }
-    
-    function showPrintOptions() {
-        try {
-            const print_config_form = document.getElementById("print-config")!;
-            print_config_form.className = "show";
-            const print_config_button = document.getElementById("print-config-btn")!;
-            print_config_button.className = "amplify-button hide";
-        } catch (e: unknown) {}
     }
     
     return (
@@ -286,120 +410,8 @@ function ControlPanel (props: {
                 )}
                 
                 <p id="info">&nbsp;</p>
-                
-                <div id={"print-exec"} className="row">
-                    <Button type="submit"  id={"print-config-btn"}
-                            className={"amplify-button amplify-field-group__control amplify-button--primary amplify-button--fullwidth btn btn-primary btn-lg"}
-                            onClick={showPrintOptions}>
-                        Show print options</Button>
-                    {/* */}
-                    {/* Print config form */}
-                    {/* */}
-                    <form id={"print-config"}>
-                        <fieldset id="config-fields">
-                            <div className="row">
-                                <div className="col-sm-4">
-                                    <div className="form-group">
-                                        <label>Unit</label><br />
-                                        <label className="radio-inline">
-                                            <input type="radio" name="unitOptions" value="in" id="inUnit" checked readOnly /> Inch
-                                        </label>
-                                        <label className="radio-inline">
-                                            <input type="radio" name="unitOptions" value="mm" id="mmUnit" readOnly /> Millimeter
-                                        </label>
-                                    </div>
-                                </div>
-                                <div className="col-sm-3">
-                                    <div className="form-group">
-                                        <label>Output format</label><br />
-                                        <label className="radio-inline">
-                                            <input type="radio" name="outputOptions" value="png" checked readOnly /> PNG
-                                        </label>
-                                        <label className="radio-inline">
-                                            <input type="radio" name="outputOptions" value="pdf" readOnly /> PDF
-                                        </label>
-                                    </div>
-                                </div>
-                                <div className="col-sm-5">
-                                    <div className="form-group">
-                                        <label htmlFor="styleSelect">Map style</label>
-                                        <select id="styleSelect" className="form-control">
-                                            <option value="mapbox://styles/mapbox/light-v9">Mapbox Light</option>
-                                            <option value="mapbox://styles/mapbox/streets-v10">Mapbox Streets</option>
-                                            <option value="https://tiles.stadiamaps.com/styles/alidade_smooth.json">Stadia Maps
-                                                Alidade Smooth
-                                            </option>
-                                            <option value="https://tiles.stadiamaps.com/styles/alidade_smooth_dark.json">Stadia
-                                                Maps Alidade Smooth Dark
-                                            </option>
-                                            <option value="https://tiles.stadiamaps.com/styles/outdoors.json">Stadia Maps
-                                                Outdoors
-                                            </option>
-                                            <option value="https://tiles.stadiamaps.com/styles/osm_bright.json">Stadia Maps OSM
-                                                Bright
-                                            </option>
-                                        </select>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="row">
-                                <div className="col-sm-2">
-                                    <div className="form-group" id="widthGroup">
-                                        <label htmlFor="widthInput">Width</label>
-                                        <input type="text" className="form-control" id="widthInput" autoComplete="off"
-                                               readOnly value="8" />
-                                    </div>
-                                </div>
-                                <div className="col-sm-2">
-                                    <div className="form-group" id="heightGroup">
-                                        <label htmlFor="heightInput">Height</label>
-                                        <input type="text" className="form-control" id="heightInput" autoComplete="off"
-                                               readOnly value="6" />
-                                    </div>
-                                </div>
-                                <div className="col-sm-3">
-                                    <div className="form-group" id="dpiGroup">
-                                        <label htmlFor="dpiInput">DPI</label>
-                                        <input type="text" className="form-control" id="dpiInput" autoComplete="off"
-                                               readOnly value="300" />
-                                    </div>
-                                </div>
-                                <div className="col-sm-5">
-                                    <div className="row">
-                                        <div className="col-sm-4">
-                                            <div className="form-group" id="latGroup">
-                                                <label htmlFor="latInput">Latitude</label>
-                                                <input type="text" className="form-control" id="latInput" autoComplete="off"
-                                                       readOnly value="" />
-                                            </div>
-                                        </div>
-                                        <div className="col-sm-4">
-                                            <div className="form-group" id="lonGroup">
-                                                <label htmlFor="lonInput">Longitude</label>
-                                                <input type="text" className="form-control" id="lonInput" autoComplete="off"
-                                                       readOnly value="" />
-                                            </div>
-                                        </div>
-                                        <div className="col-sm-4">
-                                            <div className="form-group" id="zoomGroup">
-                                                <label htmlFor="zoomInput">Zoom</label>
-                                                <input type="text" className="form-control" id="zoomInput" autoComplete="off"
-                                                       readOnly value="" />
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </fieldset>
-                    </form>
-                    
-                    <button type="submit" className={"amplify-button amplify-field-group__control amplify-button--primary amplify-button--fullwidth btn btn-primary btn-lg"} id={"generate-btn"}>Print report</button>
-                    <div id='spinner' />
-                </div>
-                
-                <a id={"data-download-link"} className="row">
-                    <button type="submit" className={"amplify-button amplify-field-group__control amplify-button--primary amplify-button--fullwidth btn btn-primary btn-lg"} id={"data-download-btn"}>Download data</button>
-                </a>
+
+                { props.children }
                 
                 <div id={"auth-control"} className="row show">
                     {(signOut !== null && typeof signOut === "function") ? (
