e37d3dad89bc8e5afd358dca62e0c49ff62ce571
John Hall <john.hall@ruralinnovation.us>
2025-02-20 13:52:03 -0500

    improve amplify build

--- src/env.d.ts
+++ src/env.d.ts
@@ -2,7 +2,6 @@
 
 interface ImportMetaEnv {
   readonly VITE_APP_VERSION: string
-  readonly VITE_AMP_BACKEND_API: string
   // readonly VITE_MAPBOX_ACCESS_TOKEN: string
   readonly VITE_MAPBOX_TOKEN: string
   readonly VITE_OFFLINE_NOTIFICATION: string
