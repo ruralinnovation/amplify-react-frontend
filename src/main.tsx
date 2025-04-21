2f2781daee89f5c8993d5d0b8b79606a787036c3
John Hall <john.hall@ruralinnovation.us>
2025-04-21 11:19:42 -0400

    pause on sso testing

--- src/main.tsx
+++ src/main.tsx
@@ -18,39 +18,39 @@ const DATA_API_URL = "https://cori-risi-apps.s3.amazonaws.com";
 
 Amplify.configure(outputs);
 
-await (signInWithRedirect({
-    provider: {
-        custom: 'IAMIdentityCenter'
-    }
-}).catch((error) => {
-    if (error instanceof AuthError) {
-        console.log(error);
-
-        if (error.name === 'UserAlreadyAuthenticatedException') {
-            // do what ever you want !!!!
-        }
-    }
-}));
+// await (signInWithRedirect({
+//     provider: {
+//         custom: 'IAMIdentityCenter'
+//     }
+// }).catch((error) => {
+//     if (error instanceof AuthError) {
+//         console.log(error);
+//
+//         if (error.name === 'UserAlreadyAuthenticatedException') {
+//             // do what ever you want !!!!
+//         }
+//     }
+// }));
 
 export function renderToDom(container: HTMLElement) {
   ReactDOM.createRoot(container).render(
       <React.StrictMode>
-          <Authenticator
-              hideSignUp={true}
-              loginMechanisms={['email']} >
-              {({ signOut, user }) => (
-                  <>
-                      <ApiContextProvider baseURL={DATA_API_URL}>
-                          <App />
-                      </ApiContextProvider>
-                      <p>To effectively sign out, first visit the&nbsp;
-                          <a href={"https://d97d22aa2db0fb35e8a5.awsapps.com/start/#/?tab=applications"} target={"_blank"}>
-                              AWS access portal</a> in a new tab and select "Sign out" from the user drop-down menu.
-                      </p>
-                      <button onClick={signOut}>Sign out</button>
-                  </>
-              )}
-          </Authenticator>
+          <ApiContextProvider baseURL={DATA_API_URL}>
+              <Authenticator
+                  hideSignUp={true}
+                  loginMechanisms={['email']} >
+                  {({ signOut, user }) => (
+                      <>
+                              <App />
+                          <p>To effectively sign out, first visit the&nbsp;
+                              <a href={"https://d97d22aa2db0fb35e8a5.awsapps.com/start/#/?tab=applications"} target={"_blank"}>
+                                  AWS access portal</a> in a new tab and select "Sign out" from the user drop-down menu.
+                          </p>
+                          <button onClick={signOut}>Sign out</button>
+                      </>
+                  )}
+              </Authenticator>
+          </ApiContextProvider>
       </React.StrictMode>
   );
 }
